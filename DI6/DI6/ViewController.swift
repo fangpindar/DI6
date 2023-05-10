6d0fddd38596c423e88cf0785108f141a1c30a57
fangpindar <fangpindar@gmail.com>
2023-05-10 12:34:44 +0800

    init

---
+++ DI6/DI6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI6
+//
+//  Created by 方品中 on 2023/5/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
